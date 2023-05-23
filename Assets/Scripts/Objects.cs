c9180a948c04def70e7135e19dbcdc4325663657
Sofija Dišlovaja <sofija.dyshlovaya@gmail.com>
2023-05-23 10:48:29 +0300

    Iesākta dragNdrop skripta rakstīšana

---
+++ Assets/Scripts/Objects.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Objects : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public Objects objectScript;			//Save pointer uz Objects script
+    CanvasGroup canvasGroup;				//
+	RectTransform objectRT;					//location
+	void Start()
+	{
+		canvasGroup = GetComponent<CanvasGroup>();	//get canvasgroup from an object
+		objectRT = GetComponent<RectTransform>();	//get recttransform from an object
+	}
+	public void OnPointerDown(PointerEventData eventData) { }
+	public void OnBeginDrag(PointerEventData eventData) { }
+	public void OnDrag(PointerEventData eventData) { }
+	public void OnEndDrag(PointerEventData eventData) { }
+}
